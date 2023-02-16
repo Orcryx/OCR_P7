1f6835c4765527940aa0ab5e418f3edd09a4eb94
Orcryx <lauryannmagar@gmail.com>
2023-02-16 17:09:50 +0100

    Nettoyer et préparer les éléments de base - Jet12

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -4,20 +4,7 @@ import './Utils/Style/App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Projet n°7 : Kasa
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Orcryx/OCR_P7"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lien vers le repo du projet
-        </a>
-      </header>
+      <h1> *-* KASA *-* </h1>
     </div>
   );
 }
