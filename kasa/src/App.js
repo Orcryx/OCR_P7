10f91bb3a90a9a0d1ed6c048e11d5a1658b274b3
Orcryx <lauryannmagar@gmail.com>
2023-02-16 15:18:32 +0100

    Initialiser le projet n° 7 : KASA

---
+++ kasa/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Projet n°7 : Kasa
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Orcryx/OCR_P7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lien vers le repo du projet
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
