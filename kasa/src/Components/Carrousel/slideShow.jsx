514b36ec88e38490db2e5383ac924a975afc07f8
Orcryx <lauryannmagar@gmail.com>
2023-02-22 17:07:32 +0100

    Composants à prévoir

---
+++ kasa/src/Components/Carrousel/slideShow.jsx
@@ -0,0 +1,10 @@
+//Importer les outils react
+import React from "react";
+//Importer le style
+import '../../Utils/Style/slideShow.css'
+import arrow from "../../Assets/Img/arrow24px.png"
+
+function Carrousel (){
+
+}
+export default Carrousel;
---
+++ kasa/src/Components/TAG/tag.jsx
@@ -0,0 +1,12 @@
+//Importer les outils react
+import React from "react";
+//Importer le style
+import '../../Utils/Style/tag.css'
+
+function TAG({tags}){
+
+    return (
+        <span className="tagSpan">{tags}</span>
+    );
+}
+export default TAG;
