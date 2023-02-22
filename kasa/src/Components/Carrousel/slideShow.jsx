dc83e6b639a01c5b3e3160f614ebe3735d1a5319
Orcryx <lauryannmagar@gmail.com>
2023-02-22 18:23:39 +0100

    Correction erreur fichier routeur et préparer le page logement:id et carrousel

--- kasa/src/Components/Carrousel/slideShow.jsx
+++ kasa/src/Components/Carrousel/slideShow.jsx
@@ -1,10 +1,53 @@
 //Importer les outils react
-import React from "react";
+import React, {useState} from "react";
 //Importer le style
-import '../../Utils/Style/slideShow.css'
-import arrow from "../../Assets/Img/arrow24px.png"
+import '../../Utils/Style/slideShow.css';
+import arrowLeft from "../../Assets/Img/leftArrow.png";
+import arrowRight from "../../Assets/Img/rightArrow.png";
 
-function Carrousel (){
+function Carrousel ({images}){
 
+    const [displayImg, setImg] = useState(0);
+    const length = images.length;
+    
+    const nextImg = () => {
+        setImg(displayImg === length - 1 ? 0 : displayImg + 1);
+      };
+    
+      const prevImg = () => {
+        setImg(displayImg === 0 ? length - 1 : displayImg - 1);
+      };
+
+      return (
+        <div className="slider">
+          {images.map((picture, index) => {
+            return (
+              <div
+                key={index}
+                className={
+                  index === displayImg
+                    ? "slide sliderActive"
+                    : "slide sliderinactive"
+                }
+              >
+                {index === displayImg && (
+                  <img src={picture} alt="" className="sliderPicture" />
+                )}
+              </div>
+            );
+          })}
+          {/* get button if there are more thant one picture */}
+          {length > 1 ? (
+            <>
+              <div className="slider__previous" onClick={prevImg}>
+                <img src={arrowLeft} alt="" className="slider__previous-icon" />
+              </div>
+              <div className="slider__next" onClick={nextImg}>
+                <img src={arrowRight} alt="" className="slider__next-icon" />
+              </div>
+            </>
+          ) : null}
+        </div>
+      );
 }
 export default Carrousel;
---
+++ kasa/src/Pages/Logement/ficheLogement.jsx
@@ -0,0 +1,18 @@
+//importer les éléments réact
+//import React from "react";
+import {useParams} from "react-router-dom";
+//Importer les éléments du projet
+import Lodging from "../../Assets/Mock/logements.json";
+
+//import SlideShow from "../../Components/Carrousel/slideShow";
+
+
+function Fiche(){
+
+//     const {lodgingId} = useParams();
+//     const lodging = Lodging.find((logement)=> logement.id === lodgingId);
+//     const {title, location, rating, host, equipements, descriptions, pictures} = lodging;
+// console.log(lodging);
+
+}
+export default Fiche;
