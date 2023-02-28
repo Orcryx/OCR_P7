cf64445672668bec65732cdd146fb373fab07d30
Orcryx <lauryannmagar@gmail.com>
2023-02-28 17:29:37 +0100

    Faire une liste avec les équipements présent dans le composant collapse de la page FicheLogement

--- kasa/src/Pages/Logement/ficheLogement.jsx
+++ kasa/src/Pages/Logement/ficheLogement.jsx
@@ -1,6 +1,6 @@
 //importer les éléments réact
 //import React from "react";
-import {useParams} from "react-router-dom";
+import {useParams, Navigate} from "react-router-dom";
 //Importer les données du projet
 import Lodging from "../../Assets/Mock/logements.json";
 //importer les éléments du projet
@@ -16,32 +16,40 @@ function Fiche(){
 
     const {lodgingId} = useParams();
     const lodging = Lodging.find(logement=> logement.id === lodgingId);
-    const {title, location, rating, host, equipments, description, pictures} = lodging;
-
-
+    const {title, location, rating, host, description, pictures} = lodging;
+    const listEquipement = lodging.equipments.map((equipment, index) =>{return<p key={index}>{equipment}</p>})
+   
     return (
-       <div className="oneLodging">
-          <SlideShow  images={pictures}/>
-          <div className="infoLodging">
-               <h1 className="titleLodging">{title}</h1>
-               <p className="localisationLodging">{location}</p>
-               <div className="infoOwerLodging">
-                    <Ower host={host}/>
-                    <Rate rates={rating}/>
-               </div>
-               <div className="tagLodging">
-                    {lodging.tags.map((tag, index) => (<TAG key={index} getTag={tag} />))}
-               </div>
-               <div className="collapseLodging">
-                    <div  className="box bigBox">
-                     <Collapse title="Description" description={description}/>
+     <>
+          {
+               lodging ? (
+                    <div className="oneLodging">
+                         <SlideShow  images={pictures}/>
+                         <div className="infoLodging">
+                              <h1 className="titleLodging">{title}</h1>
+                              <p className="localisationLodging">{location}</p>
+                              <div className="infoOwerLodging">
+                                   <Ower host={host}/>
+                                   <Rate rates={rating}/>
+                              </div>
+                              <div className="tagLodging">
+                                   {lodging.tags.map((tag, index) => (<TAG key={index} getTag={tag} />))}
+                              </div>
+                              <div className="collapseLodging">
+                                   <div  className="box bigBox">
+                                   <Collapse title="Description" description={description}/>
+                                   </div>
+                                   <div className="box smallBox">
+                                        <Collapse title="Équiments" description={listEquipement}/>
+                                   </div>
+                              </div> 
+                         </div>
                     </div>
-                    <div className="box smallBox">
-                         <Collapse title="Équiments" description={equipments}/>
-                    </div>
-               </div> 
-          </div>
-       </div>
+               ) : <Navigate to="404" replace/>
+          }
+         
+     </>
+       
       );
 }
 export default Fiche;
